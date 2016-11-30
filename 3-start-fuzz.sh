57327d116070d3a547913e4acb50516fa0ed7e50
Jarmo Puttonen <jarmo.puttonen@linux.com>
2016-11-30 21:05:23 +0200

    Start fuzzers in screen instead of tmux

--- 3-start-fuzz.sh
+++ 3-start-fuzz.sh
@@ -6,16 +6,24 @@ pkill -f afl-fuzz
 pkill -f afl-fuzz
 pkill -f afl-fuzz
 
+echo ""
+echo "[-] Starting $(nproc) fuzzers:"
+echo ""
+
 # Start master fuzzer
-tmux new-session -d "/fuzz/tmpfs/fuzz_master.sh"
+#tmux new-session -d "/fuzz/tmpfs/fuzz_master.sh"
+screen -S master -d -m /fuzz/tmpfs/fuzz_master.sh
+echo "[*] Master started"
 
 # Start slaves
 FUZZER_COUNT=$(nproc)
 
 for n in `seq -f "%02g" 2 $FUZZER_COUNT`
 do
-  tmux new-session -d "/fuzz/tmpfs/fuzz_slave.sh $n"
+  #tmux new-session -d "/fuzz/tmpfs/fuzz_slave.sh $n"
+  screen -S slave$n -d -m /fuzz/tmpfs/fuzz_slave.sh $n
+  echo "[*] Slave $n started"
   sleep 0.5
 done
 
-
+echo ""
