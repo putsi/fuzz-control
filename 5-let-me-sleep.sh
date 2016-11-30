a8cd4601ca7b352588bd72b5243475f56a8fc9fd
Jarmo Puttonen <jarmo.puttonen@linux.com>
2016-11-30 22:35:57 +0200

    Stop even more fuzzers with the sleeping script

--- 5-let-me-sleep.sh
+++ 5-let-me-sleep.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# This script installs crontab entries that will stop most of the slaves at evening and start in the morning.
-# The script should be run by the user that runs the fuzzers (not sudo and not root).
+# This script installs crontab that will stop most of the fuzzers at 23.00 and start them at 08.00.
 
+(crontab -l 2>/dev/null; echo '00 23 * * * pkill -f "slave1"') | crontab -
 (crontab -l 2>/dev/null; echo '00 23 * * * pkill -f "slave2"') | crontab -
 (crontab -l 2>/dev/null; echo '00 23 * * * pkill -f "slave3"') | crontab -
 (crontab -l 2>/dev/null; echo '00 08 * * * ~/afl-control/3-start-fuzz.sh') | crontab -
