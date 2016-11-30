5858638eb67ca6610b3bb975c9e0d7dde4a53fab
Jarmo Puttonen <jarmo.puttonen@linux.com>
2016-11-30 21:50:33 +0200

    Modify start-script to start only stopped fuzzers

--- 3-start-fuzz.sh
+++ 3-start-fuzz.sh
@@ -1,29 +1,27 @@
 #!/bin/bash
 
-# TODO: Ugly and unsafe, fix!
-pkill -f afl-fuzz
-pkill -f afl-fuzz
-pkill -f afl-fuzz
-pkill -f afl-fuzz
-
 echo ""
-echo "[-] Starting $(nproc) fuzzers:"
+echo "[-] Starting fuzzers:"
 echo ""
 
 # Start master fuzzer
-#tmux new-session -d "/fuzz/tmpfs/fuzz_master.sh"
-screen -S master -d -m /fuzz/tmpfs/fuzz_master.sh
-echo "[*] Master started"
+if ! screen -list |grep -q "master"; then
+  #tmux new-session -d "/fuzz/tmpfs/fuzz_master.sh"
+  screen -S master -d -m /fuzz/tmpfs/fuzz_master.sh
+  echo "[*] Master started"
+fi
 
 # Start slaves
 FUZZER_COUNT=$(nproc)
 
 for n in `seq -f "%02g" 2 $FUZZER_COUNT`
 do
-  #tmux new-session -d "/fuzz/tmpfs/fuzz_slave.sh $n"
-  screen -S slave$n -d -m /fuzz/tmpfs/fuzz_slave.sh $n
-  echo "[*] Slave $n started"
-  sleep 0.5
+  if ! screen -list |grep -q "slave$n"; then
+    #tmux new-session -d "/fuzz/tmpfs/fuzz_slave.sh $n"
+    screen -S slave$n -d -m /fuzz/tmpfs/fuzz_slave.sh $n
+    echo "[*] Slave $n started"
+    sleep 0.1
+  fi
 done
 
 echo ""
